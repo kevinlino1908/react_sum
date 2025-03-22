7c2fd80ccfdc6b292c3fc91e59b92135c1275e6a
kevin <kevinlino1390@gmail.com>
2025-03-22 12:37:26 -0300

    add sum and props of a and b

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,11 @@ import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <Sum
-      a={2}
-      b={3}
-    />
-    <Sum
-      a={-5}
-      b={5}
-    />
-    <Sum
-      a={10}
-      b={0}
-    />
-    <Sum
-      a={0}
-      b={5}
-    />
-    <Sum
-      a={0}
-      b={0}
-    />
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} b={0} />
+    <Sum a={0} b={5} />
+    <Sum a={0} b={0} />
     {/* Replace paragraphs with Sum componets */}
     {/* And remove commented lines :) */}
   </>
